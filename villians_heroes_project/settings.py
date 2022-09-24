3aba1c19ce061fbe375fcf16565395257a8d8a87
Paul M Vega <Paul@macbook-pro-2.lan>
2022-09-24 11:34:21 -0500

    added a few models and serilizers to the project

--- villians_heroes_project/settings.py
+++ villians_heroes_project/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'supers',
     'super_types',
-    'supers,'
+    
+    
 ]
 
 MIDDLEWARE = [
